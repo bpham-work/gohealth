b4788c5644e505642e7ec66ccc37abdeac60b421
Bryant Pham <bryantphamm@gmail.com>
2017-08-04 19:15:58 -0500

    Implement histogram parsing from file

--- src/main/java/com/gohealth/Main.java
+++ src/main/java/com/gohealth/Main.java
@@ -1,7 +1,33 @@
 package com.gohealth;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        if (args.length != 1) {
+            throw new IllegalArgumentException("You must include the path to a file to be parsed");
+        }
+
+        String filePath = args[0];
+        try {
+            List<String> linesOfText = Files.readAllLines(Paths.get(filePath));
+            HistogramParser parser = new HistogramParser();
+            Map<String, Integer> histogramMap = parser.parse(linesOfText);
+            printHistogram(histogramMap);
+        } catch (IOException e) {
+            System.out.println("Could not read file");
+        }
+    }
+
+    private static void printHistogram(Map<String, Integer> histogramMap) {
+        for (Map.Entry<String, Integer> entry : histogramMap.entrySet()) {
+            String bigram = entry.getKey();
+            Integer bigramCount = entry.getValue();
+            System.out.println(String.format("%s: %s", bigram, bigramCount));
+        }
     }
 }
